b9609d1668cb8620ea9de4487906e67e417be119
yzkonchain <yuzhoukai@onchain.com>
2021-08-11 10:50:05 +0800

    init

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+const { override, addWebpackAlias } = require("customize-cra");
+const { resolve } = require("path");
+
+module.exports = override(
+  addWebpackAlias({
+    "@": resolve("src"),
+  })
+);
---
+++ src/assets/svg/token/index.js
@@ -0,0 +1,4 @@
+import iconUsdt from './USDT.svg'
+import iconUsdc from './USDC.svg'
+
+export { iconUsdt, iconUsdc }
